3a30291590c717700c1f65f5ed0f30a0661d5873
Simon Michalowicz <simonmichalowicz@gmail.com>
2022-07-03 12:19:45 -0600

    Commiting final save from previous session

--- practice_website/pages/exercise/four/rps.js
+++ practice_website/pages/exercise/four/rps.js
@@ -1,53 +1,52 @@
-//Set player and computer score to zero
 let playerScore = 0;
 let computerScore = 0;
 let winner = ''
-//One player and one computer
-//Player selects one of the three options
-//Computer to randomly select one of the three options
+//Set scores and winner text
+
 function computerPlay(){
     let option = ['rock', 'paper', 'scissors']
     let choice = option[Math.floor(Math.random()*option.length)]
     return choice
-}
+}//function to determine computer selection
 
-//Compare results to determine winner & adjust score
 function playRound() {
     let result = ""
-    let playerSelection = window.prompt("Please choose rock, paper or scissors")
-        playerSelection = playerSelection.toLowerCase()
+
+    let playerSelection = window.prompt("Please choose rock, paper or scissors")    //player input
+        playerSelection = playerSelection.toLowerCase() //set input text to lowercase for comparison below
     
     let computerSelection = computerPlay()
-        console.log(playerSelection)
-        console.log(computerSelection)
+
+    console.log(playerSelection)
+    console.log(computerSelection)
     
     if((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rock')) { 
         playerScore += 1;
         return result = "You win! " + playerSelection + " beats " + computerSelection + "!";
-    }
+    }   //player win
 
     else if(playerSelection == computerSelection) {
         return result = "The computer also picked " + computerSelection + ". It's a tie!";
-    }
+    }   //tie game
 
     else{
         computerScore += 1;
         return result = "You lose! " + computerSelection + " beats " + playerSelection + "!";
-    }
+    }   //computer win
 
-}
+}//Function to take player selection and compare with computer selection to determine who gets a point
 
 function game() {
 
     for (let i = 0; i < 5; i++){
         console.log(playRound())
-    }
+    } //Play 5 rounds
     
-    if(playerScore > computerScore){
+    if (playerScore > computerScore){
         winner = "You win with " + playerScore + " points!" 
-    }
+    }   //Win conditions
     
-    else if(playerScore === computerScore){
+    else if (playerScore === computerScore){
         winner = "It's a tie at " + playerScore + " points!"
     }
     
@@ -57,6 +56,6 @@ function game() {
 
     console.log(winner)
 
-}
+}//Full game looping through rounds until a winner is decided and printing winner and winning score
 
 game()
