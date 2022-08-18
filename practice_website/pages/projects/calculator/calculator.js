d5f928f16807e1f173dfe90e46b2f8404104fbaa
Simon Michalowicz <simonmichalowicz@gmail.com>
2022-08-18 01:26:05 -0600

    Calculator CSS complete
    Completed CSS for calculator project, links to portfolio page to be added in at future date.
    Some more javascript changes to iron out bugs listed at base of JS file.
    Future optimisation possible and keyboard support but calculator is working as intended

--- practice_website/pages/projects/calculator/calculator.js
+++ practice_website/pages/projects/calculator/calculator.js
@@ -8,27 +8,19 @@ let operator = undefined; //operator var
 
 //basic math functions called within operate function (line 27)
 
-function add(a, b) {
-    return a + b;
-}
+function add(a, b) { return a + b; }
 
-function subtract(a, b) {
-    return a - b;
-}
+function subtract(a, b) { return a - b; }
 
-function multiply(a, b) {
-    return a * b;
-}
+function multiply(a, b) { return a * b; }
 
-function divide(a, b) {
-    return a / b;
-}
+function divide(a, b) { return a / b; }
 
 function operate(x, y, operator) {
-    x.slice(0, -1); //slice off operator attached to x
     x = parseFloat(x);  //convert both displays to floats for operate function
     y = parseFloat(y);
     if(isNaN(x) || isNaN(y)) return;
+    
     if(operator === '+') {
          result = add(x, y);
     }
@@ -40,53 +32,83 @@ function operate(x, y, operator) {
     }
     else if(operator === '/') {
         if (x === 0 || y === 0) {
-            result = "Error"
+            result = "Error";
         }
         else {
         result = divide(x, y);
         }
     }
+if(result > 99999999999999999) {
+    result = "Error";   //prevent giant numbers
+}
 return result;
 }
 
-
 function numberButton(number) {
-    if(number === '.' && lowerDisplay.innerText.includes('.')) return;  //prevent multiple decimals
+    if(number === '.' && lowerDisplay.innerText.includes('.') || currentInput > 999999999999999) return;
+        //prevent multiple decimals or numbers going outside of the display aka giant numbers
+    if( lowerDisplay.innerText === '+' || 
+        lowerDisplay.innerText === '-' || 
+        lowerDisplay.innerText === '*' || 
+        lowerDisplay.innerText === '/') 
+        {   lowerDisplay.innerText = number.toString();
+            currentInput = lowerDisplay.innerText;
+        }       //prevents multiple operators being inserted in numbers
+    else {
     currentInput = lowerDisplay.innerText + number.toString();  //number concactonation for input
+    }
 }
 
 function displayUpdate () {
     if(currentInput === undefined) {    //ends loop of undefined input if users select equals followed by an operator
         currentInput = '';
     }
+    if( lowerDisplay.innerText === '+' || 
+        lowerDisplay.innerText === '-' || 
+        lowerDisplay.innerText === '*' || 
+        lowerDisplay.innerText === '/') 
+        {return;} //prevents displays updating if only operator entered
+    else {
     lowerDisplay.innerText = currentInput;
     upperDisplay.innerText = previousInput;
+    }
 }
 
 function operatorButton (choice) {
-    if(lowerDisplay.innerText === '' || currentInput === '' || currentInput === undefined) return;    //prevent running code if either input or display is empty - prevents users pressing = button without any numbers inputted
+    if(lowerDisplay.innerText === '' || currentInput === '' || currentInput === undefined) {
+        operator = choice;
+        lowerDisplay.innerText = operator;
+        return;
+    }    //prevent running code if either input or display is empty - prevents users pressing = button without any numbers inputted
+            //sets display to operator if no numbers have been inputted
+    
     if(previousInput !== '') {      //run operate if we have two values input and an operator
-        currentInput = operate(previousInput, currentInput, choice);
+        previousInput = operate(previousInput, currentInput, choice);
+        currentInput = '';
     }
+    else {
     operator = choice;
     previousInput = currentInput + operator;    //store results to be placed in upper display via displayUpdate
-    currentInput = '';  //set current input to blanks
+    currentInput = '';
+    }  //set current input to blanks
 }
 
 function clear () {
     currentInput = '';
     previousInput = '';
     operator = undefined;
-}
+    lowerDisplay.innerText = currentInput;
+    upperDisplay.innerText = previousInput
+}       //full clear function to set all variables to default perameters
 
 function deleteNumber() {
     currentInput = currentInput.toString().slice(0, -1);
-}
+}       //delete last entered number
 
 inputButtons.forEach(button => {button.addEventListener('click', () => {    //set to listen to any button press to reduce code lines
     if (button.classList.contains('numerical')) {   //number buttons
         numberButton(button.value);
-    } 
+    }       
 
     else if (button.classList.contains('operator')) {   //operator buttons exc equals
         operatorButton(button.value);
@@ -94,11 +116,11 @@ inputButtons.forEach(button => {button.addEventListener('click', () => {    //se
 
     else if (button.classList.contains('equals')) {     //equals button
         currentInput = operate(previousInput, currentInput, operator)
-        if(currentInput === undefined) {return}     //prevents users pressing equals multiple times to cause undefined input loop
-        else {
-        previousInput = currentInput + operator;
-        currentInput = '';
-        }
+            if(currentInput === undefined) {return}     //prevents users pressing equals multiple times to cause undefined input loop
+            else {
+                previousInput = currentInput;
+                currentInput = '';
+            }
     }
 
     else if (button.classList.contains('clear')) {      //clear button to set all inputs to default values
@@ -135,7 +157,8 @@ displayUpdate();    //run at the end of every button press to update innertext d
 //only compute pairs of numbers eg 2+7-4= should run as 2+7=9 9-4=5 DONE
 //error message if dividing by 0 DONE
 //round long decimals down ALTERNATIVE OPTION BELOW
-    //Try and use CSS to have numbers flex within display to allow large number entry
+    //Try and use CSS to have numbers flex within display to allow large number entry - DONE (caused webpage crash)
+        //Disable giant numbers via javascript to prevent crashing page with giant number math. DONE
 //disable = until two numbers and operator have been entered DONE
 
 //EXTRA CREDIT
@@ -149,3 +172,6 @@ displayUpdate();    //run at the end of every button press to update innertext d
 //Fixed bug when pressing equals multiple times would lead to currentInput being set to undefined
 //Fixed bug when pressing equals followed by another operator would set currentInput to undefined
 //Fixed bug where users could attempt to divide undefined by zero causing calculator to crash
+
+//Fixed bug to prevent giant numbers being entered into calculator of more than 16 digits, this would flex the calculator and cause webpage crashing
+//Fixed bug allowing multiple operators to be entered e.g.(3+33-333+33) by changing operators before entering numbers
